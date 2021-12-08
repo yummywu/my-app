5061e5a09273e18c290fc6da0478f096aac79655
yummywu <yummywumingrui@gmail.com>
2021-12-08 21:44:18 +0800

    import antd css & ant date comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import './App.css';
+import {
+  DatePicker, message,
+} from 'antd';
+import moment from 'moment';
+
+const App: React.FC = ():React.ReactElement => {
+  const [date, setDate] = useState<moment.Moment | null>(moment(new Date()));
+
+  const handleChange = (value: moment.Moment | null) => {
+    message.info(`The time is：${value ? value.format('YYYY-MM-DD HH:mm:ss') : 'no choose'}`).then();
+    setDate(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React the 1st day
-        </a>
-      </header>
-    </div>
+      <div style={{ textAlign: 'left' }}>
+        <DatePicker onChange={handleChange} />
+        <div>
+          current date is :
+          {date ? date.format('YYYY-MM-DD HH:mm:ss') : 'no choose'}
+        </div>
+      </div>
+
   );
-}
+};
 
 export default App;
